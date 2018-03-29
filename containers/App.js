cce730d52091165448a087fce8ecb4aedb63dd93
Yahia <zaky20@hotmail.com>
2018-03-29 23:50:51 +0100

    change component to class and give state

--- containers/App.js
+++ containers/App.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react'
 
-const App = (props) => {
- 
-    return <h1>Its working</h1>
-  
+var date = dateLastWeek()
+var url = 'https://api.github.com/search/repositories?q=created:%3E' + date + '&sort=stars&order=desc'
+
+// set date minus 7 days, because we want passed weeks repos only
+// return as YYYY-MM-DD
+function dateLastWeek () {
+  var date = new Date()
+  date.setDate(date.getDate() - 7)
+  return date.toISOString().substring(0, 10)
+}
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  render () {
+    return (
+      <h1>First React Data App</h1>
+    )
+  }
 }
 
 export default App
