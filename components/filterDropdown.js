bc1fb072ef2ffbe52bf7c348f4268ab3e0be8c4f
Yahia <zaky20@hotmail.com>
2018-04-02 19:23:42 +0100

    extractLanguages function

--- components/filterDropdown.js
+++ components/filterDropdown.js
@@ -16,8 +16,24 @@ export default class FilterDropdown extends Component {
     this.setState({ selectedOption });
     console.log(`Selected: ${selectedOption.label}`);
   }
+
+  extractLanguages = (repoData) => {
+    const allRepoLanguages = repoData.map(repo => repo.language);
+    const reducedRepoLanguages = allRepoLanguages.reduce(
+    (allLanguages, currentLanguage ) => {
+      if (allLanguages.indexOf(currentLanguage) < 0 && currentLanguage !== null) allLanguages.push(currentLanguage) 
+      return allLanguages
+    } 
+  , [] )
+  return reducedRepoLanguages
+  console.log("reducedRepoLanguages", reducedRepoLanguages)
+  }
   
   render() {
+    console.log('filterdropdowndata',this.props.data)
+
+    console.log('extract function', this.extractLanguages(this.props.data) )
+
     const { selectedOption } = this.state;
     const value = selectedOption && selectedOption.value;
 
@@ -25,6 +41,7 @@ export default class FilterDropdown extends Component {
       <Select
         name="form-field-name"
         value={value}
+        placeholder={'Programming Language'}
         onChange={this.handleChange}
         options={[
           { value: 'one', label: 'One' },
