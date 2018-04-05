8bfd662fa8bf734a41b639aefbca4b4d3223d3eb
Yahia Zaky <zaky20@hotmail.com>
2018-04-05 18:56:26 +0100

    fix error for browsers with no redux dev extension installed

--- store.js
+++ store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore, compose } from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
 
 const middleware = applyMiddleware(thunk)
 
-export default createStore(rootReducer, compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+export default createStore(rootReducer, composeWithDevTools(middleware))
