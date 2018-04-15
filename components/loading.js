e738d72d77f0e5a7d132e2d3d931346a9c1bd0e6
Yahia Zaky <zaky20@hotmail.com>
2018-04-15 15:49:07 +0100

    improved loading title animation css

--- components/loading.js
+++ components/loading.js
@@ -5,15 +5,6 @@ import AnimatedPopcorn from './animatedPopcorn'
 import boyPopcorn from './images/boyPopcorn.png'
 import popcornCloud from './images/popcorn-cloud.png'
 
-// let styles ={
-//     height: "30px",
-//     width: "30px",
-//     "animation-name": "pop",
-//     "animation-duration": "1000ms",
-//     "animation-iteration-count": "infinite",
-//     "transform-origin": `${this.state.x}px ${this.state.y}px`
-// }
-
 export default class Loading extends Component {
     constructor (props) {
         super (props)
