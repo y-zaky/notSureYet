3b873e7ee62fdfe4f51596277efe709c4738f402
Yahia Zaky <zaky20@hotmail.com>
2018-04-15 20:54:49 +0100

    create isHovering state on popBasket ; in preperation of rendering a basket preview

--- components/popBasket.js
+++ components/popBasket.js
@@ -2,11 +2,40 @@ import React, {Component} from 'react'
 
 import popcornTub from './images/popcornTub.png'
 
-export default function popBasket (props) {
-    return (
-        <div className="basket__container">
-            <img className="basket__popcornImage" src={popcornTub} />
-            <p className="basket__counter">{props.starredRepos.length}</p>
-        </div>
-    )
-}
+export default class PopBasket extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            isHovering: false
+        }
+        this.handleMouseHover = this.handleMouseHover.bind(this)
+    }
+
+    handleMouseHover () {
+        console.log(this.toggleHoverState)
+        this.setState(this.toggleHoverState)
+    }
+
+    toggleHoverState (state) {
+        console.log('hi')
+        return {
+            isHovering: !state.isHovering
+        }
+    }
+
+    render () {
+        return (
+            <div className="basket__container"
+             onMouseEnter={this.handleMouseHover}
+             onMouseLeave={this.handleMouseHover}
+            >
+                <img className="basket__popcornImage" src={popcornTub} />
+                <p className="basket__counter">{this.props.starredRepos.length}</p>
+                <h1>{String(this.state.isHovering)}</h1>
+            </div>
+        )
+    }
+
+    }
+    
