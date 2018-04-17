084b0332feae21b7190efce4c8e42b1c111aa021
Yahia Zaky <zaky20@hotmail.com>
2018-04-17 18:44:01 +0100

    add link to previewBasket name

--- components/basketPreview.js
+++ components/basketPreview.js
@@ -9,7 +9,7 @@ export default class BasketPreview extends Component {
         let starredRepos = this.props.starredRepos.map(
             (repo) => (
                 <ul className={"basketPreview__repoContianer--ul"} key={repo.id}>
-                    <strong><li>{repo.name}</li></strong>
+                    <li><a href={repo.html_url}>{repo.name}</a></li>
                     <li>{repo.language || "Language Unknown"}</li>
                 </ul>
             )
