afdbf7f5d318572ffc7e2154993c3c107adbd789
Yahia Zaky <zaky20@hotmail.com>
2018-06-03 11:47:30 +0100

    saveState function for localStorage

--- localstorage.js
+++ localstorage.js
@@ -10,3 +10,13 @@ export const loadState = () => {
     return undefined;
   }
 }
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  }
+  catch (err){
+    // ignore write errors
+  }
+}
