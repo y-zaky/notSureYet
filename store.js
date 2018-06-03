a027bf959d0ef8a69ba4ceec7a512b7f70d0ec5c
Yahia Zaky <zaky20@hotmail.com>
2018-06-03 11:43:14 +0100

    loadState function for localStorage

--- store.js
+++ store.js
@@ -3,6 +3,9 @@ import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
+import {loadState, saveState} from './localstorage'
+
+const persistedState = loadState()
 
 const middleware = applyMiddleware(thunk)
 
